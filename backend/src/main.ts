6fb45d1f81fcdfdae7fa4dfaa2520211d692f442
Safiur_Rahaman_Webskitters <safiur.rahaman@webskitters.in>
2026-05-15 05:17:31 +0530

    add login for duplicate mail

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin:[process.env.FRONTEND_LOCAL_URL,  process.env.FRONTEND_URL],
     credentials: true,
   });
 
--- backend/src/modules/users/repositories/users.repository.ts
+++ backend/src/modules/users/repositories/users.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+} from "@nestjs/common";
+
 import { InjectModel } from "@nestjs/mongoose";
 import { User, UserDocument } from "../schemas/user.schema";
 import { Model } from "mongoose";
@@ -6,13 +10,24 @@ import { CreateUserDto } from "../dto/create-user.dto";
 
 @Injectable()
 export class UserRepository {
-    constructor(
-        @InjectModel(User.name) private readonly userModel: Model<UserDocument>
-    ) {}
 
-    async register(body: CreateUserDto):Promise<UserDocument> {
-        //password hash 
-        const user = await this.userModel.create(body);
-        return user;
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>
+  ) {}
+
+  async register(body: CreateUserDto): Promise<UserDocument> {
+    const existingUser = await this.userModel.findOne({
+      email: body.email,
+    });
+
+    if (existingUser) {
+      throw new BadRequestException(
+        "Email already exists"
+      );
     }
+    const user = await this.userModel.create(body);
+
+    return user;
+  }
 }
