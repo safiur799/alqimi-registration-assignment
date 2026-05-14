9530b51d2ac60db8f9bce9417a1e6661e75a39f5
Safiur_Rahaman_Webskitters <safiur.rahaman@webskitters.in>
2026-05-15 04:08:24 +0530

    build issue fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,20 @@
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-   origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   });
 
-  await app.listen(5000);
+  const PORT = process.env.PORT || 5000;
 
-  console.log(`server is running on http://localhost:5000`);
+  await app.listen(PORT);
+
+  console.log(`Server is running on port ${PORT}`);
 }
 
 bootstrap();
