3c05ac1b753d9b141f82e60a3a4b22b146b92373
Safiur_Rahaman_Webskitters <safiur.rahaman@webskitters.in>
2026-05-15 04:19:38 +0530

    config change for deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,17 @@
 import { Module } from '@nestjs/common';
-
 import { DiscoveryModule } from '@nestjs/core';
+
 import { ApiConfigModule } from './config.moudle';
 import { UsersModule } from './modules/users/users.module';
 import { UserRepositoryModule } from './modules/users/repositories/users.repository.module';
-import { ConfigModule } from '@nestjs/config';
-
-
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-        ApiConfigModule,
-        UsersModule,
-        UserRepositoryModule,
-        DiscoveryModule,
-    ],
-    providers: []
+  imports: [
+    ApiConfigModule,
+    UsersModule,
+    UserRepositoryModule,
+    DiscoveryModule,
+  ],
+  providers: [],
 })
-export class AppModule { }
- 
+export class AppModule {}
--- backend/src/config.moudle.ts
+++ backend/src/config.moudle.ts
@@ -1,36 +1,29 @@
-import { Global, Logger, Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { existsSync } from 'node:fs';
 
 @Global()
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            envFilePath: (() => {
-                let envFile = '.env';
-                if (!existsSync(envFile)) {
-                    Logger.error(`Environment file '${envFile}' not found. Please create the file`, 'ConfigModule');
-                    process.exit(1);
-                }
-                Logger.log(`Environment file '${envFile}' loaded successfully`, 'ConfigModule');
-                return envFile;
-            })(),
-            isGlobal: true
-        }),
-        MongooseModule.forRootAsync({
-            useFactory: (configService: ConfigService) => ({
-                uri: configService.getOrThrow<string>('MONGO_URI'),
-                dbName: configService.getOrThrow<string>('DB_DATABASE')
-            }),
-            inject: [ConfigService]
-        }),
-        ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }])
-    ],
-    providers: [
-        Logger
-    ]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('MONGO_URI'),
+        dbName: configService.getOrThrow<string>('DB_DATABASE'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
 })
-export class ApiConfigModule { }
- 
+export class ApiConfigModule {}
