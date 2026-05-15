9e8336e5addb50ae09c22018f922b1d67b9b4fca
Safiur_Rahaman_Webskitters <safiur.rahaman@webskitters.in>
2026-05-15 05:49:45 +0530

    migrate smtp to resend

--- backend/src/helper/mailer.helper.ts
+++ backend/src/helper/mailer.helper.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 import { ConfigService } from '@nestjs/config';
-import SMTPTransport from 'nodemailer/lib/smtp-transport';
-
 @Injectable()
 export class MailerService {
-
+    private resend: Resend;
     constructor(
         private readonly configService: ConfigService
-    ) { }
+    ) {
+        this.resend = new Resend(
+            this.configService.getOrThrow<string>(
+                'RESEND_API_KEY'
+            )
+        );
+    }
 
     async sendMail(
         from: string,
@@ -16,37 +20,16 @@ export class MailerService {
         subject: string,
         tplName: string,
         locals: any
-    ): Promise<SMTPTransport.SentMessageInfo> {
+    ) {
 
-        const user =
-            this.configService.getOrThrow<string>('MAIL_USERNAME');
-
-        const pass =
-            this.configService.getOrThrow<string>('MAIL_PASSWORD');
-
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            auth: {
-                user,
-                pass,
-            },
-            tls: {
-                rejectUnauthorized: false,
-            },
-        });
-
-        const mailOptions = {
+        return await this.resend.emails.send({
             from,
             to,
             subject,
             html: `
-        <h1>Hello ${locals.name}!</h1>
+        <h1>Hello ${locals.name}</h1>
         <p>Welcome to Forge.</p>
       `,
-        };
-
-        return await transporter.sendMail(mailOptions);
+        });
     }
 }
